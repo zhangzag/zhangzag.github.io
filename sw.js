043ac0df5dd678d17e8c9a31ed83dd656037d832
zag <mrzhangzag@163.com>
2020-05-13 17:30:26 +0800

    Site updated: 2020-05-13 17:30:25

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js')
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js')
 var cacheStorageKey = 'minimal-pwa-1'
 var cacheList = ['/', 'index.html', 'main.css', 'logo.jpg']
 self.addEventListener('install', (e) => {
