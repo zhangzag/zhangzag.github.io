92de6aac5d9b404124cab7c64e81e8daa3f9ecdf
zag <mrzhangzag@163.com>
2020-05-26 14:21:52 +0800

    png

--- js/sw.js
+++ js/sw.js
@@ -1,6 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js')
 var cacheStorageKey = 'minimal-pwa-1'
-var cacheList = ['/', '/index.html', '/css/main.css', '/fancybox/logo.jpg']
+var cacheList = ['/', '/index.html', '/css/main.css', '/fancybox/logo.png']
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches
